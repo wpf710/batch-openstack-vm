7784f4c7e0bbff3df6c7f3ac77d30c7312feb4ef
Norman <wpf710@gmail.com>
2015-12-05 10:16:31 +0800

    Update set_trust.sh

--- set_trust.sh
+++ set_trust.sh
@@ -3,7 +3,7 @@ set timeout 60
 set vm_ip [lindex $argv 0]
 set net [lindex $argv 1]
 set pd [lindex $argv 2]
-spawn ip netns exec qdhcp-$net ssh-copy-id root@$vm_ip
+spawn sed -i /$vm_ip/d /root/.ssh/known_hosts && ip netns exec qdhcp-$net ssh-copy-id root@$vm_ip
 expect "*connecting (yes/no)?"
 send "yes\r"
 expect "*password:"
